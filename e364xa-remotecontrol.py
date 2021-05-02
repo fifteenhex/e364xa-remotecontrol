7ba843185de7238c7eda1546b6d969ca981f165a
Daniel Palmer <daniel@0x0f.com>
2021-05-02 21:19:05 +0900

    Holy shit, it actually does something!

--- e364xa-remotecontrol.py
+++ e364xa-remotecontrol.py
@@ -2,25 +2,38 @@
 
 from curses import wrapper
 import argparse
-
+import time
 import easy_scpi as scpi
+import pyvisa
 
 
 class E364XA(scpi.Instrument):
 
     def __init__(self, port):
         super().__init__(port=port,
+                         timeout=500,
                          read_termination='\n',
                          write_termination='\n')
 
-    def beep(self):
-        self.system.beeper()
-
     def output_on(self):
-        self.output.state("ON")
+        try:
+            self.output.state("ON")
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        # PSU needs some time before hitting it
+        # with another command
+        time.sleep(.2)
 
     def output_off(self):
-        self.output.state("OFF")
+        try:
+            self.output.state("OFF")
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        # PSU needs some time before hitting it
+        # with another command
+        time.sleep(0.2)
 
     def output_state(self):
         return int(self.output.state())
@@ -29,10 +42,18 @@ class E364XA(scpi.Instrument):
         self.rst()
 
     def go_remote(self):
-        self.system.remote()
+        try:
+            self.system.remote()
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
 
     def go_local(self):
-        self.system.local()
+        try:
+            self.system.local()
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
 
     def get_set_voltage(self):
         return float(self.source.voltage())
@@ -43,11 +64,54 @@ class E364XA(scpi.Instrument):
     def voltage_range(self):
         return self.source.voltage.range().rstrip()
 
+    def voltage_step_up(self):
+        try:
+            self.source.voltage.level.immediate.amplitude("UP")
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        time.sleep(.2)
+
+    def voltage_step_down(self):
+        try:
+            self.source.voltage.level.immediate.amplitude("DOWN")
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        time.sleep(.2)
+
+    def voltage_step_set(self, value):
+        try:
+            self.source.voltage.level.immediate.step(value)
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        time.sleep(.2)
+
+    def current_step_up(self):
+        try:
+            self.source.current.level.immediate.amplitude("UP")
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        time.sleep(.2)
+
+    def current_step_down(self):
+        try:
+            self.source.current.level.immediate.amplitude("DOWN")
+        except pyvisa.VisaIOError:
+            # No response from command
+            pass
+        time.sleep(.2)
+
     def measure_voltage(self):
         return float(self.measure.voltage())
 
     def measure_current(self):
         return float(self.measure.current())
+
+    def get_error(self):
+        return self.error()
 
 
 def main(stdscr):
@@ -62,12 +126,11 @@ def main(stdscr):
         set_volts = inst.get_set_voltage()
         set_current = inst.get_set_current()
 
-        # inst.go_local()
-        #    inst.disconnect()
-
-        stdscr.addstr(0, 0, "%s %.3fV %.3fA (%.3fV - %s, %.3fA)" % (
+        stdscr.addstr(0, 0, "E364XA remote control")
+        stdscr.addstr(1, 0, "%s %.4fV %.4fA (%.4fV - %s, %.4fA)" % (
             on_off[output_state], volts, current, set_volts, voltage_range, set_current))
-        stdscr.addstr(1, 0, "o - On/Off, p - V step up, l - V step down")
+        stdscr.addstr(2, 0, "V Step: 1 - 1V, 2 - 0.1V, 3 - 0.01V")
+        stdscr.addstr(3, 0, "o - On/Off, p - V step up, l - V step down, a - A step up, s - A step down")
 
         stdscr.refresh()
         command = None
@@ -75,12 +138,25 @@ def main(stdscr):
             command = stdscr.getkey()
         except Exception:
             pass
-
         if command == 'o':
             if output_state == 0:
                 inst.output_on()
             else:
                 inst.output_off()
+        elif command == '1':
+            inst.voltage_step_set(1)
+        elif command == '2':
+            inst.voltage_step_set(.1)
+        elif command == '3':
+            inst.voltage_step_set(.01)
+        elif command == 'p':
+            inst.voltage_step_up()
+        elif command == 'l':
+            inst.voltage_step_down()
+        elif command == 'a':
+            inst.current_step_up()
+        elif command == 's':
+            inst.current_step_down()
 
 
 if __name__ == '__main__':
@@ -91,9 +167,10 @@ if __name__ == '__main__':
 
     inst = E364XA(args.port)
     inst.connect()
-    # inst.go_remote()
+    inst.go_remote()
 
     try:
         wrapper(main)
     except KeyboardInterrupt:
-        inst.off()
+        inst.go_local()
+        inst.disconnect()
